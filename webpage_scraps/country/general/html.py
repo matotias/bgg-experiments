b797302e38a9a9f11bcdcdb95775ec1c08edb5d6
Matias Galleguillos <matotias@gmail.com>
2020-10-26 00:33:35 -0300

    refactor(countries): merge html functions into one

--- webpage_scraps/country/general/html.py
+++ webpage_scraps/country/general/html.py
@@ -3,10 +3,6 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_country_html(url: str, headers: Dict) -> BeautifulSoup:
+def get_country_soup(url: str, headers: Dict) -> BeautifulSoup:
     response = requests.get(url, headers)
-    return get_country_soup(response)
-
-
-def get_country_soup(response: requests.Response) -> BeautifulSoup:
-    return BeautifulSoup(response.content, 'html.parser')
+    return BeautifulSoup(response.content, 'html.parser')(response)
