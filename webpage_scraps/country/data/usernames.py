feb54037fa1e5e3c3f63553754b2f71afbc12cc3
Matias Galleguillos <matotias@gmail.com>
2020-10-26 00:51:33 -0300

    feat(country.data): add wapper username function

--- webpage_scraps/country/data/usernames.py
+++ webpage_scraps/country/data/usernames.py
@@ -9,3 +9,11 @@ def get_username_list(country_soup: BeautifulSoup) -> List[BeautifulSoup]:
 
 def clean_username_text(user_text: str) -> str:
     return re.findall(r'\((.*)\)', user_text)[0].strip()
+
+
+def get_usernames(country_soup: BeautifulSoup) -> List[str]:
+    raw_usernames = get_username_list(country_soup)
+    return [
+        clean_username_text(raw_username.text)
+        for raw_username in raw_usernames
+    ]
