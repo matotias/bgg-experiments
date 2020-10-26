e3876407e9236957a5b7bd45b94fd4adfce393cf
Matias Galleguillos <matotias@gmail.com>
2020-10-25 23:49:30 -0300

    feat(country.general): add get_country_soup function

--- country/general/html.py
+++ country/general/html.py
@@ -1,6 +1,12 @@
 from typing import Dict
+from bs4 import BeautifulSoup
 import requests
 
 
-def get_country_html(url: str, headers: Dict) -> requests.Response:
-    return requests.get(url, headers)
+def get_country_html(url: str, headers: Dict) -> BeautifulSoup:
+    response = requests.get(url, headers)
+    return get_country_soup(response)
+
+
+def get_country_soup(response: requests.Response) -> BeautifulSoup:
+    return BeautifulSoup(response.content, 'html.parser')
