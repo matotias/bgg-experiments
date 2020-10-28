cb00fa260254f85a548ee1a3dd43f3bf9fe9fa86
Matias Galleguillos <matotias@gmail.com>
2020-10-28 11:16:17 -0300

    feat(webpage_scraps): getting usernames for a country returns a generator instead of having to wait for all the requests

--- webpage_scraps/country/wrappers.py
+++ webpage_scraps/country/wrappers.py
@@ -3,7 +3,7 @@ from .general.url import construct_country_url
 from .general.html import get_country_soup
 from .data.total_pages import get_total_pages
 from .data.usernames import get_usernames
-from typing import List, Callable
+from typing import List, Generator
 from time import sleep
 from random import randint
 import logging
@@ -19,17 +19,13 @@ def get_total_pages_for_country(country_name: str) -> int:
     return get_total_pages(country_soup)
 
 
-# FIXME: ALLOW IT TO DO SOMETHING IN BETWEEN CYCLES
-# TODO: maybe add an optional callable? print after each cycle and pick it up with singer?
-def get_usernames_for_country(country_name: str) -> List:
+# TODO: add generator types
+def get_usernames_for_country(country_name: str) -> Generator:
     request_parameters = read_json_file('request_parameters.json')
     total_pages = get_total_pages_for_country(country_name)
     logger.info(f'found {total_pages} pages')
-    return [
-        username
-        for page in range(1, total_pages + 1)
-        for username in slow_get_usernames_in_page(page, country_name, request_parameters)
-    ]
+    for page in range(1, total_pages + 1):
+        yield slow_get_usernames_in_page(page, country_name, request_parameters)
 
 
 def slow_get_usernames_in_page(
@@ -53,5 +49,3 @@ def get_usernames_in_page(country_name: str, page: int, request_parameters: dict
     logger.info(f'getting users from url {url}')
     country_soup = get_country_soup(url, request_parameters.get('headers', {}))
     return get_usernames(country_soup)
-
-
