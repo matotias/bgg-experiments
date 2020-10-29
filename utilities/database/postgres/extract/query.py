6ce6713dfd17e1e02f59ace8ed730720c375c3b0
Matias Galleguillos <matotias@gmail.com>
2020-10-29 00:45:50 -0300

    refactor(utilities): change postres.extraxt.query to return a list of dicts

--- utilities/database/postgres/extract/query.py
+++ utilities/database/postgres/extract/query.py
@@ -1,12 +1,12 @@
-from typing import Dict
+from typing import Dict, Any, List
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor
 
 
-def using_query(credentials: Dict[str, str], query: str) -> RealDictCursor:
+def using_query(credentials: Dict[str, str], query: str) -> List[Dict[str, Any]]:
     connection = connect(**credentials)
     cursor = connection.cursor(cursor_factory=RealDictCursor)
     cursor.execute(query)
     results = cursor.fetchall()
     connection.close()
-    return results
+    return [dict(row) for row in results]
