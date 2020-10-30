6e755b944f61d8fd05e0c36bd2b958ac77142a0f
Matias Galleguillos <matotias@gmail.com>
2020-10-29 22:08:23 -0300

    chore(primary key): add error when there is no primary key

--- utilities/database/postgres/common/primary_key.py
+++ utilities/database/postgres/common/primary_key.py
@@ -7,4 +7,6 @@ from utilities.database.postgres import extract
 def get_table_primary_key(schema: str, table: str, credentials: Dict[str, str]) -> str:
     query = Template(index_query).safe_substitute({'schema': schema, 'table': table})
     results = extract.using_query(credentials, query)
-    return results[0]['constraint_name'] if len(results) > 0 else None
+    if not len(results):
+        raise ValueError("the table doesn't have a primary key")
+    return results[0]['constraint_name']
