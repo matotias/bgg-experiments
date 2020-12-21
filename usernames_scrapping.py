f0b2fbc28d1bf9e47130ab453dd2b6bea6976690
Matias Galleguillos <matotias@gmail.com>
2020-12-21 16:28:14 -0300

    chore(scraps): use upsert to load usernames instead of insert

--- usernames_scrapping.py
+++ usernames_scrapping.py
@@ -1,6 +1,5 @@
 from webpage_scraps.country import get_usernames_for_country
 from utilities.database.postgres.upsert._upsert import upsert_data
-from psycopg2 import connect
 from utilities.files import read_json_file
 from typing import Dict
 import csv
@@ -21,25 +20,19 @@ def write_usernames_to_file(country_name) -> None:
                 writer.writerow({'username': username, 'country': country_name})
 
 
-def insert_users_to_db(connection, data, country_name):
-    query = "insert into scraps.users (username, country) values"
-    cursor = connection.cursor()
-    args_str = ','.join(cursor.mogrify(f"(%s, '{country_name}')", (x,)).decode("utf-8") for x in data)
+def insert_users_to_db(credentials, usernames, country_name):
+    data = [{'username': username, 'country': country_name} for username in usernames]
     logger.info(f"inserting chunk to database")
-    cursor.execute(query + args_str)
-    connection.commit()
+    upsert_data(data, 'scraps', 'users', credentials)
     logger.info("insertion successful")
-    cursor.close()
 
 
 def write_usernames_to_db(country_name: str, credentials: Dict[str, str], starting_page: int = 1) -> None:
     usernames = get_usernames_for_country(country_name, starting_page=starting_page)
     logger.info(f'connecting to database {credentials["database"]}')
-    connection = connect(**credentials)
     logger.info('connection successful')
     for username_chunk in usernames:
-        insert_users_to_db(connection, username_chunk, country_name)
-    connection.close()
+        insert_users_to_db(credentials, username_chunk, country_name)
 
 
-write_usernames_to_db('Chile', read_json_file('./config.json'))
+write_usernames_to_db('Spain', read_json_file('./config.json'))
