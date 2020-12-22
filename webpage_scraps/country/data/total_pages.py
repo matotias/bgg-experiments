f1cd8dd13ce64a39178259343edcf3f6b6a8cb06
Matias Galleguillos <matotias@gmail.com>
2020-12-21 21:39:56 -0300

    feat(scraps): add a return value of 5 in total pages when there is no 'last page' text in the country's webpage

--- webpage_scraps/country/data/total_pages.py
+++ webpage_scraps/country/data/total_pages.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 
 
 def get_total_pages(soup: BeautifulSoup) -> int:
+    total_pages_soup = soup.find_all("a", title="last page")
+    if len(total_pages_soup) == 0:
+        return 5
     return clean_total_pages_value(soup.find_all("a", title="last page")[0].text)
 
 
